9a3ebf34e856cadccbf4b6a2f8fe5f6a737ad788
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-04 22:36:25 -0600

    Add metadata and links

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,17 @@ import Script from 'next/script';
 import AppContext from '@context/AppContext';
 import Header from '@components/Header';
 import useInitialState from '@hooks/useInitialState';
-import "@styles/globals.css";
+import '@styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
       <Header>
+        <meta charSet="UTF-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta name="description" content="Next Shop"></meta>
+        <meta name="robots" content="index,follow"></meta>
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JCYGGMNXFL" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="UTF-8"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <meta name="description" content="Next Shop"></meta>
-        <meta name="robots" content="index,follow"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;500;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
