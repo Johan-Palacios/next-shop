7d0a8d0fe493277ce5055ed12286f205d5429027
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-04 19:08:31 -0600

    Remove unused functions

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // Containers
 import Layout from '@containers/Layout';
 // Pages
