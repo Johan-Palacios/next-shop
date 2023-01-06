aa52aee907607046b7008ace2b63a5c9a9f933f7
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-05 22:22:37 -0600

    Prevent overflow in My Order menu   - `MyOrder.module.scss` max-height defined to 400px   - `next.config.js` add domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['api.lorem.space', 'placeimg.com'],
+    domains: ['api.lorem.space', 'placeimg.com', 'cdn.shopify.com'],
   },
   // env: {
   //   customKey: 'customValue',
